285fa1755369d87486a0a4c1dd18cb1c75573e37
soldier11 <max_tab0_air_plane_039@yahoo.co.jp>
2014-02-13 00:57:16 +0900

    JPanel JLabelを使うためにぐちゃぐちゃ移行中

--- Kaisen_02.java
+++ Kaisen_02.java
@@ -1,12 +1,16 @@
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Point;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.IOException;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
 
 class Kaisen_02 extends JFrame implements KeyListener {
 	Image[] img = new Image[3];// 画像の定義用[]個作成
@@ -21,13 +25,13 @@ class Kaisen_02 extends JFrame implements KeyListener {
 	Graphics buffer;// バッファの定義
 
 	// Main
-	public static void main(String args[]) {
+	public static void main(String args[]) throws IOException {
 		new Kaisen_02();
 	}
 
 	// Constructor
-	public Kaisen_02() {
-		super("海戦ゲーム");// JFrame() 初期状態が不可視である、新しい Frameを構築します "フレームタイトル"
+	public Kaisen_02() throws IOException {
+		super("海戦ゲーム");// スーパークラスであるJFrameのコンストラクタを呼び出す "フレームタイトル"
 		img[0] = getToolkit().getImage("src/Select.png");// カーソルの画像
 		img[1] = getToolkit().getImage("src/Sea.png");// 海チップの画像
 		img[2] = getToolkit().getImage("src/LC.png");// 軽巡洋艦の画像
@@ -35,14 +39,19 @@ class Kaisen_02 extends JFrame implements KeyListener {
 		ThreadClass threadcls = new ThreadClass();// ThreadClass()のインスタンスを生成する
 		Thread thread = new Thread(threadcls);// Thread()のインスタンスを生成する
 		thread.start();// Threadを開始する
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// ウィンドウの閉じるボタンを押すとプログラムが終了するようにする
-		setSize(640, 480);// Dimension(ウィンドウ？）のサイズセットする
-		setBackground(Color.BLACK);// ウィンドウの背景色を設定する
+
+		setDefaultCloseOperation(EXIT_ON_CLOSE);// ウィンドウの閉じるボタンを押すとプログラムが終了するようにする
+		setSize(800, 600);// Dimension(ウィンドウ？）のサイズセットする
+		MainPanel p = new MainPanel();
+		getContentPane().add(p);
 		setVisible(true);// ウィンドウを描画する
-		size = getSize();// setSizeで設定したサイズをgetsizeで呼び出し，sizeと定義する
-		back = createImage(size.width, size.height);// sizeの中のwidth（幅）height（高さ）を使い，backイメージを作成
-		if (back == null)// 背景が作成されなかったらエラー分を表示する
-			System.err.print("createImage Error");
+		// getContentPane().setBackground(Color.WHITE);
+
+		// size = getSize();// setSizeで設定したサイズをgetsizeで呼び出し，sizeと定義する
+		// back = createImage(size.width, size.height);//
+		// sizeの中のwidth（幅）height（高さ）を使い，backイメージを作成
+		// if (back == null)// 背景が作成されなかったらエラー分を表示する
+		// System.err.print("createImage Error");
 	}
 
 	// Runnable Class
@@ -53,7 +62,8 @@ class Kaisen_02 extends JFrame implements KeyListener {
 				while (true) {
 					if (action())// キー入力されてカーソル移動が行われた場合
 						repaint();// paint()の再実行
-					Thread.sleep(150);// スレッドスリープによる画面の切り替えタイミングの決定
+
+					Thread.sleep(100);// スレッドスリープによる画面の切り替えタイミングの決定
 				}
 			} catch (InterruptedException e) {
 				e.printStackTrace();
@@ -62,70 +72,105 @@ class Kaisen_02 extends JFrame implements KeyListener {
 	}
 
 	// Paint Method
-	@Override
-	// java.awt.Window内のpaint()を上書き？どこで読み込んだんだ？　おそらくSwingに内包されている？
-	public void paint(Graphics g) {
-		if (back == null)
-			return;
 
-		buffer = back.getGraphics();// （back）createImageクラスの中にあるgetGraphicsメソッドを呼び出している
-		if (buffer == null)
-			return;
+	public class MainPanel extends JPanel {
 
-		//size = getSize();// サイズの呼び出し　再び？メソッド内だから？いらない？
-		buffer.setColor(getBackground());// createImage()->getGraphics()->setColor(getBackgraound())
-											// 背景をとってきて色を付けた画像を情報を取得イメージを作成する
-		buffer.fillRect(0, 0, size.width, size.height);// 指定された短形を塗りつぶす　画面の更新？
-		drawSea();
-		mapEnd();
-
-		if (phase == 0) {
-
-		} else if (phase == 1) {// 艦の召喚　可動状態
-			posLC.x = pos.x;// posLC =pos;にするとオブジェクトのコピーじゃなく，オブジェクトの参照コピーであるため×
-			posLC.y = pos.y;
-			buffer.drawImage(img[2], pos.x, pos.y, this);
-
-		} else if (phase == 2) {// 艦の投錨　固定状態へ
-			buffer.drawImage(img[2], posLC.x, posLC.y, this);
-
-		} else if (phase >= 3 && posLC.x == pos.x && posLC.y == pos.y) {// 艦の抜錨　可動状態へ
-			buffer.drawImage(img[2], pos.x, pos.y, this);
-			phase = 1;
-
-		} else if (phase >= 3) {// 艦の先端を選べていない　可動状態にするには船首を選ぶ
-			// JLabel jl=new JLabel();//TODO Jpanelをバッファに追加したい
-			buffer.drawImage(img[2], posLC.x, posLC.y, this);
-			buffer.drawString("艦の先端を選べていません。可動状態にするには船首を選んでください", 200, 20);
-			phase = 2;
+		public MainPanel() throws IOException {
+			setLayout(null);
+			setBounds(0, 0, 800, 600);
+			setBackground(Color.BLACK);
+			this.add(subPanel());
 		}
-		System.out.println(posLC + " " + pos + " " + phase);// デバック
-		buffer.drawImage(img[0], pos.x, pos.y, this);
-		
-		
-		g.drawImage(back, 0, 0, this);// gのクラス？どこ？画像全体をクラスとおいている？？bufferのこと？
-	}
 
-	public void drawSea() {// 海マップの配置 TODO　合成した画像を作り，バッファに貼るようにする
-		for (int ii = 0; ii < MAPMAX_X; ii++) {
-			for (int jj = 0; jj < MAPMAX_Y; jj++) {
-				buffer.drawImage(img[1], MAPMIN_X + 30 * ii,
-						MAPMIN_Y + 30 * jj, this);
+		public JPanel subPanel() {
+			JPanel sp = new JPanel();
+			sp.setBackground(Color.BLUE);// 会話ウインドウの色設定
+			sp.setBounds(0, 450, 784, 100);// 会話ウィンドウの描画
+			sp.setBorder(new LineBorder(Color.WHITE, 5, false));
+			return (sp);
+		}
+
+		@Override
+		// java.awt.Window内のpaint()を上書き
+		public void paintComponent(Graphics g) {// gのフィールドはここに書いてある
+			super.paintComponent(g);
+
+			// if (back == null)
+			// return;
+
+			// buffer = back.getGraphics();//
+			// （back）createImageクラスの中にあるgetGraphicsメソッドを呼び出している
+
+			// JPanel buffer = new JPanel();//
+			// （back）createImageクラスの中にあるgetGraphicsメソッドを呼び出している
+			// buffer.setBounds(0, 0,816, 600);
+
+			// if (buffer == null)
+			// return;
+
+			Font font = new Font(Font.MONOSPACED, Font.BOLD, 20); // フォントの設定・定義
+			g.setFont(font);// フォントをセットする
+			// buffer.setBackground(Color.BLUE);
+			// size = getSize();// サイズの呼び出し
+
+			// g.setColor(getBackground());//
+			// createImage()->getGraphics()->setColor(getBackgraound())
+			// 背景をとってきて色を付けた画像を情報を取得イメージを作成する
+			// g.fillRect(0, 0, size.width, size.height);// 画面の更新
+
+			drawSea(g);// 海チップの描画
+
+			mapEnd();// カーソルマップ端移動処理
+			g.setColor(Color.WHITE);
+			if (phase == 0) {// TODO 配置する艦を選択（今後実装）
+				g.drawString("配置する艦を選択してください", 18, 530);
+			} else if (phase == 1) {// 艦の召喚　可動状態
+				posLC.x = pos.x;// posLC =pos;はオブジェクトのコピーではなく参照コピーとなるため×
+				posLC.y = pos.y;
+
+				g.drawImage(img[2], pos.x, pos.y, this);
+
+			} else if (phase == 2) {// 艦の投錨　固定状態へ
+				g.drawImage(img[2], posLC.x, posLC.y, this);
+
+			} else if (phase >= 3 && posLC.x == pos.x && posLC.y == pos.y) {// 艦の抜錨　可動状態へ
+				g.drawImage(img[2], pos.x, pos.y, this);
+				phase = 1;
+
+			} else if (phase >= 3) {// 艦の先端を選べていない　可動状態にするには船首を選ぶ
+				g.drawImage(img[2], posLC.x, posLC.y, this);
+
+				g.drawString("艦の先端を選べていません。可動状態にするには船首を選んでください。", 18, 530);
+				phase = 2;
+			}
+			System.out.println(posLC + " " + pos + " " + phase);// デバック
+			g.drawImage(img[0], pos.x, pos.y, this);// カーソルの描画
+			// g.drawImage(back, 0, 0, this);//
+			// gのクラス？どこ？画像全体をクラスとおいている？？bufferのこと？
+		}
+
+		public void drawSea(Graphics g) {// 海マップの配置 TODO　合成した画像を作り，バッファに貼るようにする
+			for (int ii = 0; ii < MAPMAX_X; ii++) {
+				for (int jj = 0; jj < MAPMAX_Y; jj++) {
+					g.drawImage(img[1], MAPMIN_X + 30 * ii, MAPMIN_Y + 30 * jj,
+							this);
+				}
 			}
 		}
-	}
 
-	// カーソルが海マップをはみ出したときに反対側から戻ってくるような処理
-	public void mapEnd() {
-		if (pos.x < MAPMIN_X) {
-			pos.x += 30 * MAPMAX_X;
-		} else if (pos.x > MAPMIN_X + 30 * (MAPMAX_X - 1)) {
-			pos.x -= 30 * MAPMAX_X;
-		} else if (pos.y < MAPMIN_Y) {
-			pos.y += 30 * MAPMAX_Y;
-		} else if (pos.y > MAPMIN_Y + 30 * (MAPMAX_Y - 1)) {
-			pos.y -= 30 * MAPMAX_Y;
+		// カーソルが海マップをはみ出したときに反対側から戻ってくるような処理
+		public void mapEnd() {
+			if (pos.x < MAPMIN_X) {
+				pos.x += 30 * MAPMAX_X;
+			} else if (pos.x > MAPMIN_X + 30 * (MAPMAX_X - 1)) {
+				pos.x -= 30 * MAPMAX_X;
+			} else if (pos.y < MAPMIN_Y) {
+				pos.y += 30 * MAPMAX_Y;
+			} else if (pos.y > MAPMIN_Y + 30 * (MAPMAX_Y - 1)) {
+				pos.y -= 30 * MAPMAX_Y;
+			}
 		}
+
 	}
 
 	// カーソルの移動
